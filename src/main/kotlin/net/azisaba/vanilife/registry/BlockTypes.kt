440b3995117f879bba6b4876384752aefb75151c
tksimeji <tksimeji@outlook.com>
2025-03-01 12:53:54 +0900

    chore: changed the class name of the test block

--- src/main/kotlin/net/azisaba/vanilife/registry/BlockTypes.kt
+++ src/main/kotlin/net/azisaba/vanilife/registry/BlockTypes.kt
@@ -1,8 +1,8 @@
 package net.azisaba.vanilife.registry
 
 import net.azisaba.vanilife.block.CustomBlockType
-import net.azisaba.vanilife.block.TestBlock
+import net.azisaba.vanilife.block.TestBlockType
 
 object BlockTypes: KeyedRegistry<CustomBlockType>() {
-    val TEST = register(TestBlock)
+    val TEST = register(TestBlockType)
 }
