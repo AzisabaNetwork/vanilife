0624dc986a1859a69e5f878548946ec50c4b11c1
tksimeji <tksimeji@outlook.com>
2025-03-20 09:40:58 +0900

    initial commit

---
+++ src/main/kotlin/net/azisaba/vanilife/Vanilife.kt
@@ -0,0 +1,11 @@
+package net.azisaba.vanilife
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Vanilife : JavaPlugin() {
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+    }
+}
---
+++ src/main/kotlin/net/azisaba/vanilife/VanilifeBootstrap.kt
@@ -0,0 +1,9 @@
+package net.azisaba.vanilife
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap
+
+class VanilifeBootstrap: PluginBootstrap {
+    override fun bootstrap(ctx: BootstrapContext) {
+    }
+}
